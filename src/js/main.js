92e7cc08277e69957ea9ecce3ffb1d97e1222bc7
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-11-14 19:57:38 +0000

    fix: nav menu

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+var header = document.querySelector('.header');
+var logo = document.querySelector('.logo');
+var searchBar = document.querySelector('.search-bar');
+var loginButton = document.querySelector('.login-button');
+
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+
+    // Oculta a barra de pesquisa e o botão de login quando rolar para baixo
+    if (scrollPosition > 0) {
+        header.classList.add('hidden');
+    } else {
+        header.classList.remove('hidden');
+    }
+});
