91d35065f2676340f350c1aa912034d0530e3e4c
Ramon-az <rla.mgr21@uea.edu.br>
2023-11-16 16:03:53 -0400

    Apenas mais alguns testes com o menu e restante do header. Nada definitivo!

--- src/js/testeEfeitomenu.js
+++ src/js/testeEfeitomenu.js
@@ -1,6 +1,9 @@
 window.addEventListener("scroll", function(){
     let header = document.querySelector('.nav-menu')
     header.classList.toggle('rolagem', window.scrollY > 0)
+    /*if(window.scrollY > 0){
+        const text = document.querySelector('.text').remove();
+    }*/
 });
 
 /*
