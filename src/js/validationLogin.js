73c4b707a9fe126d8d83474271efe6da27a73fe5
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-11-23 20:29:11 +0000

    feat: adicionando DOM na área de login

--- src/js/validationLogin.js
+++ src/js/validationLogin.js
@@ -1,3 +1,7 @@
-let inputUser = document.getElementById ('user');
-let inputPassword = document.getElementById ('password');
+let user = document.getElementById('user');
+let password = document.getElementById('password');
+let submit = document.getElementById('submit');
 
+console.log(user)
+console.log(password)
+console.log(submit)
