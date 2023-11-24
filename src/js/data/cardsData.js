37aaee2cbb03dae6e6af4277f9b3af9a5de5f620
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-11-24 20:04:41 +0000

    feat: esqueleto dos cards em destaque

---
+++ src/js/data/cardsData.js
@@ -0,0 +1,38 @@
+export const cardsData = [
+    {
+        "id": "1",
+        "title": "",
+        "description": "",
+        "photoCover": ""
+    },
+    {
+        "id": "2",
+        "title": "",
+        "description": "",
+        "photoCover": ""
+    },
+    {
+        "id": "3",
+        "title": "",
+        "description": "",
+        "photoCover": ""
+    },
+    {
+        "id": "4",
+        "title": "",
+        "description": "",
+        "photoCover": ""
+    },
+    {
+        "id": "5",
+        "title": "",
+        "description": "",
+        "photoCover": ""
+    },
+    {
+        "id": "6",
+        "title": "",
+        "description": "",
+        "photoCover": ""
+    }
+]
