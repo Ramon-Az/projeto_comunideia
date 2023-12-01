4a9671c34194ea50d94a2377e4f5e76510466862
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-12-01 17:29:18 +0000

    style: dando estilo aos cards

--- .idea/readme.md
+++ .idea/readme.md
@@ -1,3 +1,5 @@
 ### Links Úteis
 
 Icons: [Font Awesome](https://fontawesome.com/icons)
+
+Logos: [Pexels] https://www.pexels.com/pt-br/procurar/logotipos/
--- src/js/data/cardsData.js
+++ src/js/data/cardsData.js
@@ -1,63 +1,63 @@
 export const cardsData = [
     {
-        "photoCard": "././assets/img/default-image.jpg",
+        "photoCard": "././assets/img/pexels/pexels-frank-cone.jpg",
+        "title": "Fire Bar",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
+        
+    },
+    {
+        "photoCard": "././assets/img/pexels/pexels-pixabay.jpg",
+        "title": "Lorem Ipsum",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
+    },
+    {
+        "photoCard": "././assets/img/pexels/pexels-magda-ehlers.jpg",
+        "title": "Lorem Ipsum",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
+    },
+    {
+        "photoCard": "././assets/img/pexels/pexels-pixabay-2.jpg",
+        "title": "Lorem Ipsum",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
+    },
+    {
+        "photoCard": "././assets/img/pexels/pexels-stephen-niemeier.jpg",
         "title": "Lorem Ipsum",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
         
     },
     {
-        "photoCard": "././assets/img/default-image.jpg",
+        "photoCard": "././assets/img/pexels/pexels-matheus-bertelli.jpg",
         "title": "Lorem Ipsum",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
     },
     {
-        "photoCard": "././assets/img/default-image.jpg",
+        "photoCard": "././assets/img/pexels/pexels-miguel-montejano.jpg",
         "title": "Lorem Ipsum",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
     },
     {
-        "photoCard": "././assets/img/default-image.jpg",
+        "photoCard": "././assets/img/pexels/pexels-dids.jpg",
         "title": "Lorem Ipsum",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
     },
     {
-        "photoCard": "././assets/img/default-image.jpg",
-        "title": "Lorem Ipsum",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
-        
-    },
-    {
-        "photoCard": "././assets/img/default-image.jpg",
+        "photoCard": "././assets/img/pexels/pexels-expect.jpg",
         "title": "Lorem Ipsum",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
     },
     {
-        "photoCard": "././assets/img/default-image.jpg",
+        "photoCard": "././assets/img/pexels/pexels-henry-acevedo.jpg",
         "title": "Lorem Ipsum",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
     },
     {
-        "photoCard": "././assets/img/default-image.jpg",
+        "photoCard": "././assets/img/pexels/pexels-magda-ehlers-2.jpg",
         "title": "Lorem Ipsum",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
     },
     {
-        "photoCard": "././assets/img/default-image.jpg",
-        "title": "Lorem Ipsum",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
-    },
-    {
-        "photoCard": "././assets/img/default-image.jpg",
-        "title": "Lorem Ipsum",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
-    },
-    {
-        "photoCard": "././assets/img/default-image.jpg",
-        "title": "Lorem Ipsum",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
-    },
-    {
-        "photoCard": "././assets/img/default-image.jpg",
+        "photoCard": "././assets/img/pexels/pexels-kei-scampa.jpg",
         "title": "Lorem Ipsum",
         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dolor risus, pharetra eu diam id, luctus condimentum mi."
     }
