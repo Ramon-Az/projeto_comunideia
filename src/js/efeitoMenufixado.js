965e798dd360c45389f9f7d62ba4e940adce6948
Ramon-az <rla.mgr21@uea.edu.br>
2023-12-06 16:03:38 -0400

    fiz muita alteração que nem lembro todas, mas foram só melhorias!

--- src/js/efeitoMenufixado.js
+++ src/js/efeitoMenufixado.js
@@ -1,15 +1,14 @@
 console.log("conectado");
 window.addEventListener("scroll", function () {
-    let header = document.querySelector("nav");
-    console.log(header)
-    if (window.scrollY > 0) {
-        console.log("executando scroll");
-        header.classList.add("rolagem");
-        // document.querySelector(".text").style.display = "none";
+  let header = document.querySelector("nav");
+  console.log(header)
+  if (window.scrollY > 0) {
+    console.log("executando scroll");
+    header.classList.add("rolagem");
+    // document.querySelector(".text").style.display = "none";
   } else {
-        console.log("Início do scroll");
-        header.classList.remove("rolagem");
-         // document.querySelector(".text").style.display = "block";
+    console.log("Início do scroll");
+    header.classList.remove("rolagem");
+    // document.querySelector(".text").style.display = "block";
   }
 });
-
