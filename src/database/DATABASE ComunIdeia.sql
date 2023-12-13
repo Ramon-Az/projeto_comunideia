04f71e79827bde2f0c79a12d24f75f17e7c8722c
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-12-13 19:00:39 +0000

    fix: melhorias ao banco

--- src/database/DATABASE ComunIdeia.sql
+++ src/database/DATABASE ComunIdeia.sql
@@ -1,18 +1,83 @@
 CREATE DATABASE comunideia;
 USE comunideia;
 
--- ================= tb_cadastro =================
-CREATE TABLE cadastro (
-    usuario_id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    sobrenome VARCHAR(100) NOT NULL,
-    nome_usuario VARCHAR(50) UNIQUE NOT NULL,
-    data_nascimento DATE NOT NULL,
-    cpf VARCHAR(14) UNIQUE NOT NULL,
-    telefone INT NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    senha VARCHAR(255) NOT NULL
+-- ================= ENUMs =================
+
+CREATE TYPE "dias" AS ENUM (
+  'segunda-feira',
+  'terça-feira',
+  'quarta-feira',
+  'quinta-feira',
+  'sexta-feira',
+  'sábado',
+  'domingo'
 );
+
+CREATE TYPE "tipo_de_plano" AS ENUM (
+  'básico',
+  'premium',
+  'plus'
+);
+
+-- ================= tb_cadastro =================
+
+CREATE TABLE "cadastro" (
+  "usuario_id" SERIAL PRIMARY KEY,
+  "nome" VARCHAR(100) NOT NULL,
+  "sobrenome" VARCHAR(100) NOT NULL,
+  "nome_usuario" VARCHAR(50) UNIQUE NOT NULL,
+  "data_nascimento" DATE NOT NULL,
+  "cpf" VARCHAR(14) UNIQUE NOT NULL,
+  "telefone" INT NOT NULL,
+  "email" VARCHAR(100) NOT NULL,
+  "senha" VARCHAR(255) NOT NULL
+);
+
+-- ================= tb_cadastro_empresa =================
+
+CREATE TABLE "cadastro_empresa" (
+  "empresa_id" SERIAL PRIMARY KEY,
+  "fk_usuario_id" INTEGER,
+  "photoCard" VARCHAR(255) NOT NULL,
+  "title" VARCHAR(100) UNIQUE NOT NULL,
+  "descriptions" VARCHAR(255) NOT NULL,
+  "localityRoad" VARCHAR(100) NOT NULL,
+  "localityDistrict" VARCHAR(60) NOT NULL,
+  "localityState" VARCHAR(60) NOT NULL,
+  "dias_funcionamento" dias NOT NULL,
+  "horario_inicio" TIME NOT NULL,
+  "horario_termino" TIME NOT NULL,
+  "telefone" VARCHAR(15) NOT NULL,
+  "site_da_empresa" VARCHAR(100),
+  "social_instagram" VARCHAR(100),
+  "social_linkedin" VARCHAR(100),
+  "social_facebook" VARCHAR(100),
+  "social_twitter" VARCHAR(100)
+);
+
+-- ================= tb_faq =================
+
+CREATE TABLE "FAQ" (
+  "faq_id" SERIAL PRIMARY KEY,
+  "nome" VARCHAR(100) NOT NULL,
+  "email" VARCHAR(100) NOT NULL,
+  "descricao" VARCHAR(255) NOT NULL
+);
+
+-- ================= tb_planos =================
+
+CREATE TABLE "planos" (
+  "plano_id" SERIAL PRIMARY KEY,
+  "fk_usuario_id" INTEGER,
+  "planos" tipo_de_plano UNIQUE NOT NULL
+);
+
+ALTER TABLE "cadastro_empresa" ADD FOREIGN KEY ("fk_usuario_id") REFERENCES "cadastro" ("usuario_id");
+
+ALTER TABLE "planos" ADD FOREIGN KEY ("fk_usuario_id") REFERENCES "cadastro" ("usuario_id");
+
+
+-- ================= INSERINDO DADOS =================
 
 INSERT INTO 
 	cadastro(nome, sobrenome,nome_usuario, data_nascimento, cpf, telefone, email, senha)
@@ -20,31 +85,6 @@ VALUES
 	('Lucas', 'Ton Gonçalves', 'Minerva', '20000513', '99999999999', 981567254, 'lucaston2323@gmail.com', 'admin123');
 
 SELECT * FROM cadastro;
-
-
--- ================= tb_cadastro_empresa =================
-CREATE TYPE dias AS ENUM 
-	('segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado', 'domingo');
-
-CREATE TABLE cadastro_empresa (
-    empresa_id SERIAL PRIMARY KEY,
-  	fk_usuario_id INTEGER REFERENCES cadastro(usuario_id),
-    photoCard VARCHAR(255) NOT NULL,
-    title VARCHAR(100) UNIQUE NOT NULL,
-    descriptions VARCHAR(255) NOT NULL,
-    localityRoad VARCHAR(100) NOT NULL,
-    localityDistrict VARCHAR(60) NOT NULL,
-  	localityState VARCHAR(60) NOT NULL,
-    dias_funcionamento dias[] NOT NULL,
-    horario_inicio TIME NOT NULL,
-  	horario_termino TIME NOT NULL,
-    telefone VARCHAR(15) NOT NULL,
-    site_da_empresa VARCHAR(100),
-    social_instagram VARCHAR(100),
-    social_linkedin VARCHAR(100),
-    social_facebook VARCHAR(100),
-    social_twitter VARCHAR(100)
-);
 
 INSERT INTO 
 	cadastro_empresa (dias_funcionamento)
@@ -71,22 +111,4 @@ VALUES
 
 
 SELECT * FROM cadastro_empresa;
-
--- ================= tb_faq =================
-
-CREATE TABLE FAQ (
-    ID SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    descricao VARCHAR(255) NOT NULL
-);
-
--- ================= tb_planos =================
-
-CREATE TYPE tipo_de_plano AS ENUM ('básico', 'premium', 'plus');
-
-CREATE TABLE planos (
-    id SERIAL PRIMARY KEY,
-    planos tipo_de_plano UNIQUE NOT NULL
-);
 
